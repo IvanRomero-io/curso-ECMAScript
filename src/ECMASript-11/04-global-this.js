45d5fdb441f1a041b0f64d7c3f13d36f55e90a68
IvanRomero-io <ivandejesusrl2001@gmail.com>
2023-02-18 13:40:12 -0500

    global this

---
+++ src/ECMASript-11/04-global-this.js
@@ -0,0 +1,4 @@
+console.log(window) //En la consola del VSC no devuelve nada porque window sirve es en los navegadores
+console.log(global) //Muestra en las consola las funciones de node
+console.log(self) //webworker
+console.log(globalThis) //informacion segun el contexto
