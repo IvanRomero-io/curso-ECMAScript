2c4d70cd8637aef8a02d4ca85d14d8f64d9997a4
IvanRomero-io <ivandejesusrl2001@gmail.com>
2023-02-20 11:30:31 -0500

    replace all

---
+++ src/ECMASript-12/01-replace-all.js
@@ -0,0 +1,6 @@
+const stringwow = "JavaScript es un maravilloso lenguage de programacion"
+
+const replacedString = stringwow.replace('JavaScript', "Java") //Este metodo sirve para buscar una palabra en una variable con un string y reemplazarla
+
+console.log(replacedString)
+
